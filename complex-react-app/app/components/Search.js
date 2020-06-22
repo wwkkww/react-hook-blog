51d9e3340d8e886f50f96a11862a05c87f98a14f
kevin <kevin@betacode.io>
2020-06-22 14:15:34 +0800

    add loading spinner while searching

--- complex-react-app/app/components/Search.js
+++ complex-react-app/app/components/Search.js
@@ -1,8 +1,16 @@
 import React, { useEffect, useContext } from 'react';
+import { useImmer } from 'use-immer';
 import DispatchContext from '../DispatchContext';
+import Axios from 'axios';
 
 function Search() {
   const appDispatch = useContext(DispatchContext);
+  const [state, setState] = useImmer({
+    searchTerm: '',
+    results: [],
+    show: 'neither', // ['result', 'searching', 'neither']
+    requestCount: 0,
+  });
 
   // componentDidMount, add keyboard listener
   useEffect(() => {
@@ -11,12 +19,71 @@ function Search() {
     return () => document.removeEventListener('keyup', searchKeyPress);
   }, []);
 
+  useEffect(() => {
+    if (state.searchTerm.trim()) {
+      setState(draft => {
+        draft.show = 'searching';
+      });
+      const delay = setTimeout(() => {
+        console.log(state.searchTerm);
+        // send search request here after user stop typing
+        setState(draft => {
+          draft.requestCount++;
+        });
+      }, 1000);
+
+      // clarTimeout will run everytime this useEffect is called
+      // in this case of typing, every previous setTimeout will be remove and
+      // triggered again when searchterm is changed
+      return () => clearTimeout(delay);
+    } else {
+      setState(draft => {
+        draft.show = 'neither';
+      });
+    }
+  }, [state.searchTerm]);
+
+  useEffect(() => {
+    if (state.requestCount) {
+      // send axios request here
+      const ourRequest = Axios.CancelToken.source();
+      async function fetchResult() {
+        try {
+          const response = await Axios.post(
+            '/search',
+            { searchTerm: state.searchTerm },
+            { cancelToken: ourRequest.token }
+          );
+
+          console.log(response.data);
+          setState(draft => {
+            draft.results = response.data;
+            draft.show = 'results';
+          });
+        } catch (error) {
+          console.log('error fetch result', error);
+        }
+      }
+
+      fetchResult();
+
+      return () => ourRequest.cancel();
+    }
+  }, [state.requestCount]);
+
   function searchKeyPress(e) {
     console.log('key pressed', e.keyCode);
 
     if (e.keyCode === 27) {
       appDispatch({ type: 'closeSearch' });
     }
+  }
+
+  function handleInput(e) {
+    const value = e.target.value;
+    setState(draft => {
+      draft.searchTerm = value;
+    });
   }
 
   return (
@@ -28,6 +95,7 @@ function Search() {
               <i className="fas fa-search"></i>
             </label>
             <input
+              onChange={handleInput}
               autoFocus
               type="text"
               autoComplete="off"
@@ -46,7 +114,17 @@ function Search() {
 
         <div className="search-overlay-bottom">
           <div className="container container--narrow py-3">
-            <div className="live-search-results live-search-results--visible">
+            <div
+              className={
+                'circle-loader ' + (state.show === 'searching' ? 'circle-loader--visible' : '')
+              }
+            ></div>
+            <div
+              className={
+                'live-search-results ' +
+                (state.show === 'results' ? 'live-search-results--visible' : '')
+              }
+            >
               <div className="list-group shadow-sm">
                 <div className="list-group-item active">
                   <strong>Search Results</strong> (3 items found)
