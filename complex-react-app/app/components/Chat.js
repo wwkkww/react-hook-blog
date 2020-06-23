117a75e5e8ee774c162b8093e26c08d2457ee060
kevin <kevin@betacode.io>
2020-06-23 17:23:53 +0800

    add socket.emit & socket.on to send and receive chat from server

--- complex-react-app/app/components/Chat.js
+++ complex-react-app/app/components/Chat.js
@@ -1,10 +1,61 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useRef } from 'react';
+import { useImmer } from 'use-immer';
+import io from 'socket.io-client';
+import { Link } from 'react-router-dom';
 import StateContext from '../StateContext';
 import DispatchContext from '../DispatchContext';
 
+// setup url point to server & establish ongoing connection between clien & server
+const socket = io('http://localhost:8080');
+
 function Chat() {
+  const chatField = useRef(null);
   const appState = useContext(StateContext);
   const appDispatch = useContext(DispatchContext);
+  const [state, setState] = useImmer({
+    fieldValue: '',
+    chatMessages: [],
+  });
+
+  useEffect(() => {
+    if (appState.isChatOpen) {
+      chatField.current.focus();
+    }
+  }, [appState.isChatOpen]);
+
+  useEffect(() => {
+    socket.on('chatFromServer', message => {
+      setState(draft => {
+        draft.chatMessages.push(message);
+      });
+    });
+  }, []);
+
+  function handleFieldChange(e) {
+    const value = e.target.value;
+    setState(draft => {
+      draft.fieldValue = value;
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('submit', state.fieldValue);
+    // Send message to chat server
+    socket.emit('chatFromBrowser', {
+      message: state.fieldValue,
+      token: appState.user.token,
+    });
+    setState(draft => {
+      // add msg to chatMessages[]
+      draft.chatMessages.push({
+        message: state.fieldValue,
+        username: appState.user.username,
+        avatar: appState.user.avatar,
+      });
+      draft.fieldValue = '';
+    });
+  }
 
   return (
     <div
@@ -21,40 +72,45 @@ function Chat() {
         </span>
       </div>
       <div id="chat" className="chat-log">
-        <div className="chat-self">
-          <div className="chat-message">
-            <div className="chat-message-inner">Hey, how are you?</div>
-          </div>
-          <img
-            className="chat-avatar avatar-tiny"
-            src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-          />
-        </div>
+        {state.chatMessages.map((message, index) => {
+          if (message.username === appState.user.username) {
+            return (
+              <div key={index} className="chat-self">
+                <div className="chat-message">
+                  <div className="chat-message-inner">{message.message}</div>
+                </div>
+                <img className="chat-avatar avatar-tiny" src={message.avatar} />
+              </div>
+            );
+          }
 
-        <div className="chat-other">
-          <a href="#">
-            <img
-              className="avatar-tiny"
-              src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"
-            />
-          </a>
-          <div className="chat-message">
-            <div className="chat-message-inner">
-              <a href="#">
-                <strong>barksalot:</strong>
-              </a>
-              Hey, I am good, how about you?
+          return (
+            <div key={index} className="chat-other">
+              <Link to={`/profile/${message.username}`}>
+                <img className="avatar-tiny" src={message.avatar} />
+              </Link>
+              <div className="chat-message">
+                <div className="chat-message-inner">
+                  <Link to={`/profile/${message.username}`}>
+                    <strong>{message.username}: </strong>
+                  </Link>
+                  {message.message}
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
-      <form id="chatForm" className="chat-form border-top">
+      <form id="chatForm" className="chat-form border-top" onSubmit={handleSubmit}>
         <input
+          ref={chatField}
           type="text"
           className="chat-field"
           id="chatField"
           placeholder="Type a message…"
           autoComplete="off"
+          onChange={handleFieldChange}
+          value={state.fieldValue}
         />
       </form>
     </div>
