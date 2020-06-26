d69d44ab9f9a84416afa6f158936d6ab39009c46
kevin <kevin@betacode.io>
2020-06-26 12:13:29 +0800

    add babel & generate index html from jsx

--- complex-react-app/app/components/Header.js
+++ complex-react-app/app/components/Header.js
@@ -4,8 +4,10 @@ import HeaderLoggedOut from './HeaderLoggedOut';
 import HeaderLoggedIn from './HeaderLoggedIn';
 import StateContext from '../StateContext';
 
-function Header() {
+function Header(props) {
   const appState = useContext(StateContext);
+
+  const headerContent = appState.loggedIn ? <HeaderLoggedIn /> : <HeaderLoggedOut />;
 
   return (
     <header className="header-bar bg-primary mb-3">
@@ -15,7 +17,9 @@ function Header() {
             ComplexApp
           </Link>
         </h4>
-        {appState.loggedIn ? <HeaderLoggedIn /> : <HeaderLoggedOut />}
+        {/* {appState.loggedIn ? <HeaderLoggedIn /> : <HeaderLoggedOut />} */}
+
+        {!props.staticEmpty ? headerContent : ''}
       </div>
     </header>
   );
